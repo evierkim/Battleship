f19d55eb228368d9e795ad1abd826affec0221c0
annabellee <anlee@potomacschool.org>
2021-10-18 09:56:04 -0400

    0ct 14 class

--- HumanPlayer.py
+++ HumanPlayer.py
@@ -10,22 +10,43 @@ class HumanPlayer:
         #compare shots against the other player's ship grid
 
     def placeShip(self, ship, size):
-        #use createShipGrid
-        #depending on the size, ask what column/row and place the ship
-        #confirm that it's a legal placement using isSpaceWater
-        direction = input("Input 0 for horizontal, 1 for vertical :")
-        row = int(input("Input row 0-9:"))
-        column = int(input("Input column 0-9:"))
-        if direction == 0: #if horizontal placement
-            for x in range (column, column+size):
-                if isSpaceWater(self, row, x) == False:
+        placed = True
+        while placed:
 
-            self.gridShips.changeRow(self, row, ship, column, size)
-        else: #if vertical placement
-            for x in range (row, row+size):
-                if isSpaceWater(self, column, x) == False:
+            direction = input("Input 0 for horizontal, 1 for vertical")
+            row = int(input("Input row 0-9:"))
+            column = int(input("Input column 0-9:"))
 
-            self.gridShips.changeCol(self, column, ship, row, size)
+            if direction == 0: #if horizontal placement
+                allSpacesOpen = False
+
+                for x in range(column, column+size): #loops through the row, "size" number of times
+                    if self.gridShips.isSpaceWater(self, row, x) == False: #if the space is not open/water
+                        allSpacesOpen = False
+                        break
+                    else:
+                        allSpacesOpen = True
+
+                if allSpacesOpen is True:
+                    self.gridShips.changeRow(self, row, ship, column, size)
+                placed = False
+
+            else: #if vertical placement
+                allSpacesOpen = False
+
+                for x in range(row, row+size):
+                    if self.gridShips.isSpaceWater(self, x, row) == False: #if the space is not open/water
+                        allSpacesOpen = False
+                        break
+                    else:
+                        allSpacesOpen = True
+
+                if allSpacesOpen is True:
+                    self.gridShips.changeCol(self, column, ship, row, size)
+
+
+    #def stillHasShips(self, row, column):
+        #if isSpaceWater(self, row, column) == False: #if the space
 
 
 
