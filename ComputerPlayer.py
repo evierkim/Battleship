c09ee807eb120b5d16abb90aa24a94cf620202fc
Evie <erkim@potomaschool.org>
2021-11-03 17:06:27 -0400

    11/3

--- ComputerPlayer.py
+++ ComputerPlayer.py
@@ -3,6 +3,17 @@ import random
 class ComputerPlayer(Player):
     def __init__(self):
         super().__init__()
+        self.pHit = False # tells us if the previous turn was a hit
+        self.shotHit = False # tells us if the current shot just made was a hit
+        self.direction = 0
+        self.r = 0
+        self.c = 0
+        self.belowOpen = False
+        self.aboveOpen = False
+        self.rightOpen = False
+        self.leftOpen = False
+        self.count = 0
+        self.countMax = 4
     """
     placeShip
     randomly places parameter ship in clear spaces
@@ -32,27 +43,88 @@ class ComputerPlayer(Player):
                         break
                 if shipNotPlace == False: # area is clear
                     self.gridShips.changeCol(c, ship, r, size)
-
+    """
+    shot
+    shoots at space
+    self.shotHit is True if ship is hit, False if miss
+    """
+    def shot(self, otherPlayer, r, c):
+        if otherPlayer.gridShips.returnLocation(r, c) == "~":  # if shot at water
+            otherPlayer.gridShips.changeSingleSpace(r, c, "o")
+            self.gridShots.changeSingleSpace(r, c, "o")
+            self.shotHit = False
+        elif otherPlayer.gridShips.returnLocation(r, c) != "~":  # if shot at ship
+            otherPlayer.gridShips.changeSingleSpace(r, c, "x")
+            self.gridShots.changeSingleSpace(r, c, "x")
+            print("Hit")
+            self.shotHit = True
+    """
+    checkSpaces
+    checks the surrounding spaces to see if they are water and might have a ship
+    """
+    def checkSpaces(self):
+        if self.r != 9 and self.gridShots.returnLocation(self.r + self.count, self.c) == "~":
+            self.belowOpen = True
+        if self.r != 0 and self.gridShots.returnLocation(self.r - self.count, self.c) == "~":
+            self.aboveOpen = True
+        if self.c != 0 and self.gridShots.returnLocation(self.r, self.c - self.count) == "~":
+            self.leftOpen = True
+        if self.c != 9 and self.gridShots.returnLocation(self.r, self.c + self.count) == "~":
+            self.rightOpen = True
     """
     takeTurn
-    takes shot at random new space
+    takes shot at new space
     notifies if ship has been hit/sunk
     @param  otherPlayer  Player object of opposing player
     """
     def takeTurn(self,otherPlayer):
-        b = True
-        while b: # runs until new index is generated
-            r = random.randrange(0, 10)
-            c = random.randrange(0, 10)
-            if otherPlayer.gridShips.returnLocation(r,c) != "o" and otherPlayer.gridShips.returnLocation(r,c) != "x": # if space has not been shot at
-                b = False
-        if otherPlayer.gridShips.returnLocation(r,c) == "~": # if shot at water
-            otherPlayer.gridShips.changeSingleSpace(r,c,"o")
-            self.gridShots.changeSingleSpace(r,c,"o")
-        elif otherPlayer.gridShips.returnLocation(r,c) != "~": # if shot at ship
-            otherPlayer.gridShips.changeSingleSpace(r,c,"x")
-            self.gridShots.changeSingleSpace(r,c,"x")
-            print("Hit")
+        if self.pHit: # if the previous turn hit a ship and it's not sunk yet
+            self.checkSpaces()
+            self.count += 1
+            if (self.belowOpen == False and self.direction == 0) or self.count > self.countMax: # if space below isn't open
+                self.direction = 2
+                self.count = 0
+            if (self.aboveOpen == False and self.direction == 2) or self.count > self.countMax: # if space above isn't open
+                self.direction = 1
+                self.count = 0
+            if (self.leftOpen == False and self.direction == 1) or self.count > self.countMax: # if space to left isn't open
+                self.direction = 3
+                self.count = 0
+            """ this will never be true/run
+            if (self.rightOpen == False and self.direction == 3) or self.count > self.countMax: # if space to right isn't open
+                self.direction = 0
+                self.count = 0
+            """
+            if self.direction == 0: # space below
+                self.shot(otherPlayer, self.r + self.count, self.c) # takes shot below
+                if self.shotHit is False:  # miss
+                    self.direction = 2 # changes direction to above
+                    self.count = 0
+            elif self.direction == 2:  # space above
+                self.shot(otherPlayer, self.r - self.count, self.c)
+                if self.shotHit is False:  # miss
+                    self.direction = 1
+                    self.count = 0
+            elif self.direction == 1:  # space to left
+                self.shot(otherPlayer,self.r, self.c - self.count)
+                if self.shotHit is False:  # miss
+                    self.direction = 3
+                    self.count = 0
+            elif self.direction == 3:  # space to right
+                self.shot(otherPlayer,self.r, self.c + self.count)
+                """ this will never be true/run
+                if self.shotHit is False:  # miss
+                    self.direction = 0
+                    self.count = 0
+                """
+        else: # previous turn did not hit a ship5
+            needNewSpace = True
+            while needNewSpace: # runs until new index is generated
+                self.r = random.randrange(0, 10)
+                self.c = random.randrange(0, 10)
+                if self.gridShots.returnLocation(self.r,self.c) == "o": # if space has not been shot at
+                    self.shot(otherPlayer, self.r, self.c)
+                    needNewSpace = False
         self.printGrids()
         a = 0
         b = 0
@@ -65,22 +137,45 @@ class ComputerPlayer(Player):
                     a += 1
                 elif otherPlayer.gridShips.returnLocation(x,y) == "B": # there is a battleship
                     b += 1
-                elif otherPlayer.gridShips.returnLocation(x, y) == "C": # there is a cruiser
+                elif otherPlayer.gridShips.returnLocation(x,y) == "C": # there is a cruiser
                     c += 1
-                elif otherPlayer.gridShips.returnLocation(x, y) == "D": # there is a destroyer
+                elif otherPlayer.gridShips.returnLocation(x,y) == "D": # there is a destroyer
                     d += 1
-                elif otherPlayer.gridShips.returnLocation(x, y) == "S": # there is a sub
+                elif otherPlayer.gridShips.returnLocation(x,y) == "S": # there is a sub
                     s += 1
         if a == 0: # aircraft sunk
             print("aircraft carrier sunk")
+            self.pHit = False
+            self.direction = 0
+            self.count = 0
+            self.countMax = 3
         if b == 0: # battleship sunk
             print("battleship sunk")
+            self.pHit = False
+            self.direction = 0
+            self.count = 0
+            if a == 0: # aircraft and battleship sunk
+                self.countMax = 2
         if c == 0: # cruiser sunk
             print("cruiser sunk")
+            self.pHit = False
+            self.direction = 0
+            self.count = 0
+            self.countMax = 1
+            if a == 0 and b == 0 and s == 0: # aircraft, battleship, cruiser, and sub sunk
+                self.countMax = 1
         if d == 0: # destroyer sunk
             print("destroyer sunk")
+            self.pHit = False
+            self.direction = 0
+            self.count = 0
         if s == 0: # sub sunk
             print("submarine sunk")
+            self.pHit = False
+            self.direction = 0
+            self.count = 0
+            if a == 0 and b == 0 and c == 0: # aircraft, battleship, cruiser, and sub sunk
+                self.countMax = 1
     """
     stillHasShips
     determines if you still have ships on your board
--- Game.py
+++ Game.py
@@ -20,3 +20,41 @@ else:
     print("You Won!")
 
 print("Game over")
+
+"""
+        if self.hit == True: # previous turn hit ship
+            if self.r >= 9: # bottom element
+                self.direction = 1
+            elif self.r <= 0: # top element
+                self.direction = 2
+            elif self.c >= 9: # rightmost element
+                self.direction = 3
+            elif self.c <= 0: # leftmost element
+                self.direction = 0
+            # account for corner elements - evie
+            if self.direction == 0 and self.r != 9: # space below
+                if self.gridShots.returnLocation(self.r+1,self.c) == "~": # space below is not shot at
+                    #self.shot(otherPlayer, self.r + 1, self.c)
+                    if self.shot(otherPlayer,self.r+1,self.c) == False: # miss
+                        self.direction = 1
+            elif self.direction == 1 and self.r+1 >= 0: # space above
+                if self.gridShots.returnLocation(self.r-1,self.c) == "~": # space above is not shot at
+                    #self.shot(otherPlayer, self.r - 1, self.c)
+                    if self.shot(otherPlayer,self.r - 1, self.c) == False: # miss
+                        self.direction = 2
+            elif self.direction == 2 and self.c >= 0: # space to left
+                if self.gridShots.returnLocation(self.r, self.c - 1) == "~":  # space to left is not shot at
+                    #self.shot(otherPlayer,self.r, self.c - 1)
+                    if self.shot(otherPlayer,self.r, self.c - 1) == False: # miss
+                        self.direction = 3
+            elif self.direction == 3 and self.c <= 9: # space to right
+                if self.gridShots.returnLocation(self.r,self.c+1) == "~": # space to right is not shot at
+                    #self.shot(otherPlayer,self.r, self.c + 1)
+                    if self.shot(otherPlayer,self.r, self.c + 1) == False: # miss
+                        self.hit == False
+                        self.direction = 0
+
+
+
+        else: # previous turn did not hit ship
+"""
